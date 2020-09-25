d9c889349c63f9dfcf907e5ed31956067c6d5fda
David Draguta <daviddraguta@gmail.com>
2020-09-25 16:49:12 -0400

    Some progress.

--- src/book.c
+++ src/book.c
@@ -7,25 +7,48 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#include "book.h"
+
+int
+get_data(char *s, char *val){
+  printf("Enter the books %s: \n", val);
+  fgets(s, 25, stdin);
+  return 1;
+}
+
+book
+new_book(char* title, char* author, char* year, char* publisher, char* symbolic_link)
+{
+  book b = malloc(sizeof(char*) * 5);
+  strcp(b.title, title);
+  strcp(b.author, author);
+  strcp(b.year, year);
+  strcp(b.publisher, publisher);
+  strcp(b.symbolic_link, symbolic_link);
+
+  return b;
+}
+
+
 int add_book()
 {
-  const char* name =      malloc(sizeof(char) * 25);
-  const char* author =    malloc(sizeof(char) * 25);
-  const char* year =      malloc(sizeof(char) * 25);
-  const char* publisher = malloc(sizeof(char) * 25);
-  const char* final     = malloc(sizeof(char) * 100);
-  const char *temp1     = malloc(sizeof(char)*  50);
-  const char *temp2     = malloc(sizeof(char)*  50);
-  const struct stat st  = {0};
+  const char* title         =         malloc(sizeof(char) * 25);
+  const char* author        =         malloc(sizeof(char) * 25);
+  const char* year          =         malloc(sizeof(char) * 25);
+  const char* publisher     =         malloc(sizeof(char) * 25);
+  const char* symbolic_link =         malloc(sizeof(char) * 25);
+  const char* final         =         malloc(sizeof(char) * 100);
+  const char *temp1         =         malloc(sizeof(char)*  50);
+  const char *temp2         =         malloc(sizeof(char)*  50);
+  const struct stat st      =         {0};
+  book b;
 
   int i;
-
-  
   for(i=0;i<4;i++)
     switch(i){
     case 0:
-      get_data("name", name);
-      strcat(final, name);
+      get_data("title", title);
+      strcat(final, title);
       break;
     case 1:
       get_data("author", author);
@@ -39,7 +62,18 @@ int add_book()
       get_data("publisher", publisher);
       strcat(final, publisher);
       break;
+    case 4:
+      get_data("symbolic link", symbolic_link);
+      strcat(final, symbolic_link);
+      break;
     }
+  b = new_book(title, author, year, publisher, symbolic_link);
+
+  free(title);
+  free(author);
+  free(year);
+  free(publisher);
+  free(symbolic_link);
 
   i=0;
   while(final[i+1]){
@@ -47,11 +81,11 @@ int add_book()
       final[i] = '-';
     i++;
   }
-  //mkdir books/name/notes/ books/name/exercises/
+  //mkdir books/title/notes/ books/title/exercises/
   //echo final >> ../info
 
   strcat(temp1, "/books/");
-  strcat(temp1, name);
+  strcat(temp1, b.title);
   remove_trailing_newline(temp1);
   
   if(stat(temp1, &st) == -1){
@@ -69,11 +103,17 @@ int add_book()
 
 }
 
-int remove_book()
+int
+symbolically_link(char* symbolic_link){
+}
+
+int
+remove_book(char* name, char* author)
 {
 }
 
-int list_books()
+int
+list_books()
 {
 }
 
---
+++ src/book.h
@@ -0,0 +1,9 @@
+
+struct book {
+  char* name = malloc(sizeof(char) * 25);
+  char* author = malloc(sizeof(char) * 25);
+  char* year = malloc(sizeof(char) * 25);
+  char* publisher = malloc(sizeof(char) * 25);
+  char* symbolic_link = malloc(sizeof(char) * 25);
+}
+  
