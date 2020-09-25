587863f58e7622aef567af5544559a654bc2355a
David Draguta <daviddraguta@gmail.com>
2020-09-25 14:50:52 -0400

    This will be the basis of generalizing the operation.

--- algorithms/src/main.c
+++ algorithms/src/main.c
@@ -2,10 +2,24 @@
 #include <stdio.h>
 #include <string.h>
 
-void
-helper(char *inp, char *s){
-  printf("Enter the books %s: \n", inp);
+//to mkdir in program
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int
+helper(char *val, char *s){
+  printf("Enter the books %s: \n", val);
   fgets(s, 25, stdin);
+  return 1;
+}
+int
+remove_trailing_newline(char *s)
+{
+  while(*s)
+    if(*(s++) == '\n')
+      *(s-1) = '\0';
+  return -1;
 }
 
 int main()
@@ -45,10 +59,30 @@ int main()
   }
   //mkdir books/name/notes/ books/name/exercises/
   //echo final >> ../info
+  struct stat st = {0};
+  char *temp1 = malloc(sizeof(name) + (sizeof(char) * 19));
+  char *temp2 = malloc(sizeof(name) + (sizeof(char) * 19));
+  strcat(temp1, "../books/");
+  strcat(temp1, name);
+  remove_trailing_newline(temp1);
+  
+  if(stat(temp1, &st) == -1){
+    printf("temp1: %s", temp1);
+    mkdir(temp1, 0700);
+    strcpy(temp2 , temp1);
+    strcat(temp1, "/notes");
+    strcat(temp2,"/exercises");
+    printf("temp1: %s \n", temp1);
+    printf("temp2: %s \n", temp2);
+    mkdir(temp1, 0700);
+    mkdir(temp2, 0700);
+  }
 
   free(name);
   free(author);
   free(year);
   free(publisher);
   free(final);
+  free(temp2);
+  free(temp1);
 }
