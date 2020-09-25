a441fa1be9564e98fda9ca3169ab4c76709b7583
David Draguta <daviddraguta@gmail.com>
2020-09-25 15:30:53 -0400

    Basis of program.

---
+++ mksubject.sh
@@ -0,0 +1,5 @@
+#The program to add books needs to be echoed from here into src, but for now I will work on it in src, then generalize it out here.
+mkdir -p $1/src &&
+mkdir $1/books/ &&
+touch $1/src/main.c 
+
